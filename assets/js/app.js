35883bb09e7c9cea87a3f1f4dbf8e6e4b7f9cc35
Myles Reid <myles@reidsmailbox.com>
2024-11-27 10:42:20 -0600

    Typing animation added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,36 @@
 'use strict';
+
+import * as utils from './utils.js';
+
+const phrase = utils.select('.phrase');
+
+
+let swapablePhrase = phrase.textContent;
+const rotatingPhrases = [
+  'a developer.', 'enthusiastic.', 'Myles Reid.', 'a student.'
+];
+
+function textTypingEffect(element, text, i = 0) {
+  if (i === 0) element.textContent = '';
+  element.textContent += text[i]
+  if (i === text.length - 1) return;
+  setTimeout(() => textTypingEffect(element, text, i + 1), 75);
+}
+
+function changePhrase(i) {
+  const newPhrase = rotatingPhrases[i];
+  textTypingEffect(phrase, newPhrase); 
+}
+
+function rotatePhrase() {
+  let i = 0;
+  setInterval(() => {
+    changePhrase(i);
+    i = (i + 1) % rotatingPhrases.length; 
+  }, 3000);
+}
+
+utils.listen('load', window, () => {
+  rotatePhrase()
+  textTypingEffect(phrase, swapablePhrase);
+});
