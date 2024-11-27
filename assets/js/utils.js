ab36eacc136b3569b07d64fcb0faa4621ea0a893
Myles Reid <myles@reidsmailbox.com>
2024-11-27 09:14:56 -0600

    Initilizing files

---
+++ assets/js/utils.js
@@ -0,0 +1,35 @@
+export function select(selector, scope = document) {
+  return scope.querySelector(selector);
+}
+
+export function selectAll(selector, scope = document) {
+  return scope.querySelectorAll(selector);
+}
+
+export function listen(event, element, callback) {
+  return element.addEventListener(event, callback);
+}
+
+export function create(element, scope = document) {
+  return scope.createElement(element);
+}
+
+export function addChild(element, child) {
+  return element.appendChild(child);
+}
+
+export function addFirstChild(element, child) {
+  return element.insertBefore(child, element.firstChild);
+}
+
+export function addClass(element, text) {
+  return element.classList.add(text);
+}
+
+export function removeClass(element, text) {
+  return element.classList.remove(text);
+}
+
+export function toggleClass(element, text) {
+  return element.classList.toggle(text);
+}
